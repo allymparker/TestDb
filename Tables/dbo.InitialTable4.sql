e2601c0c1b8a8dcd1523462ec62f40ba80197a47
RedgateSchemaHistory <schemahistory@red-gate.com>
2020-09-16 12:52:19 +0000

    Schema history update

--- Tables/dbo.InitialTable4.sql
+++ Tables/dbo.InitialTable4.sql
@@ -1,9 +1,9 @@
-CREATE TABLE [dbo].[InitialTable1]
+CREATE TABLE [dbo].[InitialTable4]
 (
 [Id] [int] NOT NULL,
 [ColumnName2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ColumnName3] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 )
 GO
-ALTER TABLE [dbo].[InitialTable1] ADD CONSTRAINT [PK__InitialT__3214EC07710208A6] PRIMARY KEY CLUSTERED  ([Id])
+ALTER TABLE [dbo].[InitialTable4] ADD CONSTRAINT [PK__InitialT__3214EC07104AC753] PRIMARY KEY CLUSTERED  ([Id])
 GO
