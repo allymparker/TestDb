cc5e6fce1f0a485ad8372c90eb45c67d0ecb4fc1
RedgateSchemaHistory <schemahistory@red-gate.com>
2020-09-25 08:19:14 +0000

    Schema history update

---
+++ Tables/dbo.MyTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[MyTable]
+(
+[Id] [int] NOT NULL,
+[ColumnName2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ColumnName3] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+)
+GO
+ALTER TABLE [dbo].[MyTable] ADD CONSTRAINT [PK__MyTable__3214EC07CC56A276] PRIMARY KEY CLUSTERED  ([Id])
+GO
